b70946ce8a230db603845cc1f06569c0eb6f400d
rino-san <makirino@amazon.co.jp>
2024-11-03 18:38:35 +0900

    complete 3-4

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -6,10 +6,14 @@ type Props = {
 };
 
 export function TodoItem({todoItem}:Props){
+    const onDeleteButton = () =>{
+        console.log(`Delete task: ${todoItem.task}`)
+    };
     return(
         <li>
             <input type ="checkbox" readOnly checked={todoItem.completed}/>
             {todoItem.task}
+            <button onClick={onDeleteButton}>削除</button>
         </li>
     )
 }
